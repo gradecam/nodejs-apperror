69a29251a3d815a58b2702846bedfaebc146c2c6
Jarom Loveridge <jaroml@gradecam.com>
2015-08-19 07:07:03 -0600

    Bump version and some code cleanup.

--- apperror.js
+++ apperror.js
@@ -1,110 +1,69 @@
 /* vim: set softtabstop=4 ts=4 sw=4 expandtab tw=120 syntax=javascript: */
-/* jshint node:true, unused:true */
 'use strict';
 
 module.exports = AppError;
 
 var util = require('util');
 
-var DEFAULT_ERROR_LEVEL = 'error';
-var DEFAULT_LOGGER = console.log;
+var _ = require('lodash');
+
+var DEFAULT_LOGGER = console;
 
 function AppError(opts, context) {
-    if (!isError(this)) { return new AppError(opts, context); }
+    if (!(this instanceof Error)) { return new AppError(opts, context); }
     opts = opts || {};
-    if (isString(opts)) { opts = {message: opts} };
-    this.type = opts.type || this.constructor.name;
+    if (_.isString(opts)) { opts = {message: opts}; }
     this.message = opts.message || 'An error occurred.';
     this.data = opts.data || '';
-    this.code = opts.code || opts.statusCode || 500;
+    this.code = opts.code || opts.status || opts.statusCode || 500;
     this.statusCode = this.code;
     this.isAppError = true;
-    this.logError = isUndefined(opts.logError) && true || opts.logError;
-    this.captureStack = isUndefined(opts.captureStack) && true || opts.captureStack;
-    if (opts.captureStack) {
-        Error.captureStackTrace(this, (context || arguments.callee));
+    this.logError = _.isUndefined(opts.logError) && true || opts.logError;
+    this.captureStack = _.isUndefined(opts.captureStack) && true || opts.captureStack;
+    if (this.captureStack) {
+        Error.captureStackTrace(this, (context || AppError));
     }
     this._logged = false;
-    return this;
 }
 util.inherits(AppError, Error);
 AppError.prototype.name = 'AppError';
-AppError.prototype.toJSON = toJSON;
-AppError.prototype.toResponseObject = toJSON;
-AppError.prototype.log = function log(level, logger) {
-    logger = logger || this.DEFAULT_LOGGER || DEFAULT_LOGGER;
-    level = level || this.DEFAULT_ERROR_LEVEL || DEFAULT_ERROR_LEVEL;
-    if (this.logError && this._logged || !isFunction(logger)) {
-        return this;
-    }
-    logger(level.toUpperCase(), this.toJSON());
-    this._logged = true;
-    return this;
-}
-
-function toJSON() {
+AppError.prototype.toJSON = AppError.prototype.toResponseObject = function toJSON() {
     return {
-        type: this.type,
+        type: this.name,
         message: this.message,
         data: this.data,
         code: this.code,
     };
+
+};
+
+AppError.prototype.log = function log(logger) {
+    logger = logger || this.DEFAULT_LOGGER || DEFAULT_LOGGER;
+    if (!this.logError || this.logError && this._logged) {
+        return this;
+    }
+    logger.error(this.toJSON());
+    this._logged = true;
+    return this;
 };
 
 AppError.createCustom = function createCustom(name, defaults) {
     function CustomError(msg, code, data) {
-        if (!isError(this)) { return new CustomError(msg, code, data); }
-        var opts = clone(defaults);
-        if (isObject(msg)) {
+        if (!(this instanceof Error)) { return new CustomError(msg, code, data); }
+        var opts = _.extend({}, defaults);
+        if (_.isObject(msg)) {
             opts.message = msg.message || msg.msg || opts.message || opts.msg;
-            opts.code = msg.code || opts.code || opts.statusCode;
+            opts.code = msg.code || msg.status || msg.statusCode || opts.code || opts.status || opts.statusCode;
             opts.data = msg.data || opts.data;
         } else {
             opts.message = msg || opts.message || opts.msg;
-            opts.code = code || opts.code || opts.statusCode;
+            opts.code = code || opts.code || opts.status || opts.statusCode;
             opts.data = data || opts.data;
         }
         AppError.call(this, opts, CustomError);
-        return this;
     }
     util.inherits(CustomError, AppError);
     CustomError.prototype.name = name;
 
     return CustomError;
-}
-
-var _has = Object.prototype.hasOwnProperty;
-
-function clone(obj) {
-    var o = {}, key;
-    var keys = Object.getOwnPropertyNames(obj);
-    for (var i=0, len=keys.length; i<len; i++) {
-        key = keys[i];
-        o[key] = obj[key];
-    }
-    return o;
-}
-
-function has(obj, prop) {
-    return _has.call(obj, prop);
-}
-
-function isString(value) {
-    return 'string' === typeof value;
-}
-
-function isError(value) {
-    return value instanceof Error;
-}
-
-function isFunction(value) {
-    return 'function' === typeof value;
-}
-
-function isObject(value) {
-    return value && 'object' === typeof value;
-}
-
-function isUndefined(value) {
-    return 'undefined' === typeof value;
-}
+};
